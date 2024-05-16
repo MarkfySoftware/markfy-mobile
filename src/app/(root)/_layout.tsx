c24c27f4813a0bc5e878f79506427ea65d61121c
gfrnca <gabrielbfranca27@gmail.com>
2024-05-16 15:54:25 -0300

    feat: tabs to root layout

--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -1,10 +1,29 @@
-import { Stack } from 'expo-router'
+import Colors from '@/src/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
 import React from 'react'
 
 export default function HomeLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors.primary,
+                tabBarInactiveTintColor: 'gray',
+                tabBarItemStyle: { height: 70 }, // Aumentar a altura das abas
+                tabBarLabelStyle: { fontSize: 24 }, // Aumentar o tamanho da fonte das labels das abas
+                tabBarIconStyle: { width: 40, height: 40 }, // Ajustar o tamanho do ícone
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" color={color} size={size} /> // Ajuste o tamanho do ícone
+                    ),
+                }}
+            />
+        </Tabs>
     )
 }
